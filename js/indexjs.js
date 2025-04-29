9690592b03a419d7d326c633c5a039aaf8894015
srbech <srbechler@gmail.com>
2025-04-29 11:13:44 -0400

    fun facts

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,5 +1,17 @@
-const rain = document.querySelector("#rain");
+const funfact_one = document.querySelector("#funfact_one");
 
-rain.onclick = function (){
-    alert("You selected rain!")
+funfact_one.onclick = function (){
+    alert("This is inspired by a food truck that would come to my rec sporting events.")
+}
+
+const funfact_two = document.querySelector("#funfact_two");
+
+funfact_two.onclick = function (){
+    alert("I almost went with an ice cream truck instead.")
+}
+
+const funfact_three = document.querySelector("#funfact_three");
+
+funfact_three.onclick = function (){
+    alert("I think food trucks are great.")
 }
