bd2fe99916717c18c8befb0cd471b53ac55a4be2
srbech <srbechler@gmail.com>
2025-05-09 20:15:52 -0400

    fun fact if-else

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,17 +1,29 @@
+let funfact_oneClicked = false;
+let funfact_twoClicked = false;
+
 const funfact_one = document.querySelector("#funfact_one");
-
-funfact_one.onclick = function (){
-    alert("This is inspired by a food truck that would come to my rec sporting events.")
-}
-
 const funfact_two = document.querySelector("#funfact_two");
-
-funfact_two.onclick = function (){
-    alert("I almost went with an ice cream truck instead.")
-}
-
 const funfact_three = document.querySelector("#funfact_three");
 
-funfact_three.onclick = function (){
-    alert("I think food trucks are great.")
+funfact_one.onclick = function () {
+
+        if (funfact_twoClicked) {
+            alert("Oh no, fun fact 2 was clicked!");
+        } else {
+            alert("I think food trucks are great.");
+        }
+        funfact_oneClicked = true;
+    }
+
+funfact_two.onclick = function () {
+    alert("I almost went with an ice cream truck instead.");
+    funfact_twoClicked = true;
+}
+
+funfact_three.onclick = function () {
+    if (funfact_oneClicked) {
+        alert("Oops! Fun fact 1 was clicked first!");
+    } else {
+        alert("I think food trucks are great.");
+    }
 }
