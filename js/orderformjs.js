1da80ab3ba22db09945f78fffb4c84dd0ae026e5
srbech <srbechler@gmail.com>
2025-05-11 17:02:54 -0400

    order form fix

--- js/orderformjs.js
+++ js/orderformjs.js
@@ -1,15 +1,19 @@
-const first_name = document.querySelector("first_name");
-const submit = document.querySelector("submit");
+const first_name = document.querySelector("#first-name");
+const submit = document.querySelector("#submit");
 const clear = document.querySelector("#clear");
+const heading = document.querySelector("#form_heading");
 
 first_name.oninput = function () {
-    heading.innerHTML = first_name.value + "'s Order Form"
+    heading.innerHTML = first_name.value + "'s Order Form";
 }
 
-submit.onclick = function () {
+submit.onclick = function (event) {
+    event.preventDefault();
     alert("Thank you for ordering!");
 }
 
-clear.onclick = function () {
+clear.onclick = function (event) {
+    event.preventDefault();
     alert("All inputs have been cleared.");
+    document.querySelector("form").reset();
 }
