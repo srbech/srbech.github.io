4b65ccca12b00b392c5730caf9cf25f432db1f91
srbech <srbechler@gmail.com>
2025-05-11 20:17:37 -0400

    fav dessert fix

--- js/menujs.js
+++ js/menujs.js
@@ -2,7 +2,7 @@ let fav_entreeClicked = false;
 
 const fav_entree = document.querySelector("#fav_entree");
 const fav_side = document.querySelector("#fav_side");
-const fav_desert = document.querySelector("#fav_desert");
+const fav_dessert = document.querySelector("#fav_dessert");
 
 fav_entree.onclick = function () {
     alert("Burgers!");
@@ -13,7 +13,7 @@ fav_side.onclick = function () {
     alert("French Fries!");
 }
 
-fav_desert.onclick = function () {
+fav_dessert.onclick = function () {
     if (fav_entreeClicked = true) {
         alert("I personally like a good cookie with my burger and fries, but people prefer ice cream.");
     } else {
